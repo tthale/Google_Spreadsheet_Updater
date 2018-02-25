e13592c7952c4f6375e489feece1f547eec3f520
tthale <tthale02@gmail.com>
2018-02-25 15:21:05 -0800

    Added in system environment functionality & spec up by row-writing. Overall, made more efficient

--- fill_spreadsheet.py
+++ fill_spreadsheet.py
@@ -1,10 +1,12 @@
 import gspread
+import pygsheets
 import random
 import sys
 import time
 import array
+import googleapiclient
 from oauth2client.service_account import ServiceAccountCredentials
-
+import os
 #test statements
 '''
 print(chr(0))
@@ -14,8 +16,15 @@ print(ord(chr(65)))
 w = (sys.argv)
 name = (w[1])
 wksn = (w[2])
-ed = 'tthale02@gmail.com'
+ed = os.environ['google_spreadsheet_sharing']
+sec = os.environ['secrets']
 
+value = w
+for v in range(3):
+	value.pop(0)
+	
+#Old version using gspread
+'''
 #wksn = ((time.strftime("%m/%d/%Y")))
 name_old = str(2872116359)
 
@@ -26,13 +35,17 @@ name_old = str(2872116359)
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds',
 'https://www.googleapis.com/auth/drive']
+
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+
 client = gspread.authorize(creds)
 gc = gspread.authorize(creds)
+'''
+#These are used to authorize any functions accessing google spreadsheets
+#gc = pygsheets.authorize(something, no_cache = True)
+gc = pygsheets.authorize(sec, no_cache=True)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-#(temp) sheet = client.open("Survey_Project_Responses").sheet1
+
 ''' temporary opening: '''
 #sh = gc.open(name_old)
 
@@ -41,33 +54,23 @@ gc = gspread.authorize(creds)
 #This is to create a spreadsheet if it cannot be opened
 try:
 	gc.open(name)
-except gspread.exceptions.SpreadsheetNotFound:
+except pygsheets.SpreadsheetNotFound:
 
-	sh = gc.create(name)
-	sh.share(ed, perm_type='user', role='writer')
+	sh = gc.create(name, parent_id = None)
+	sh.share(ed, role='writer')
 
-gc.open(name)
-#for worksheet creation
+#for worksheet editing
 sh = gc.open(name)
-
-#For the parsing of the row
-sh2 = gc.open(name).sheet1
 
 #This is to create a worksheet if a worksheet cannot be opened
 try:
-	sh.worksheet(wksn)
+	sh.worksheet_by_title(wksn)
 
-except gspread.exceptions.WorksheetNotFound:
-	worksheet = sh.add_worksheet(title=str(wksn), rows="100", cols="26")
+except pygsheets.WorksheetNotFound:
+	worksheet = sh.add_worksheet(title=str(wksn), rows="100", cols="26", src_tuple=None, src_worksheet=None)
 
-sh.worksheet(wksn)
-
+wks = sh.worksheet_by_title(wksn)
 ''' end of stuff '''
-
-wks = sh.worksheet(wksn)
-wks2 = client.open(name).sheet1
-
-n2 = 1
 
 #Old Example Array
 '''
@@ -80,7 +83,8 @@ c2 = 0
 line = 1
 n = 0
 l = ['cell1','cell2','cell3','cell4','cell5','cell6','cell7','cell8','cell9','cell10']
-
+'''
+Temporary Commenting Out
 if ((wks.row_values(d)[0]) == ''):
 	for i in range(len(l)):
 		if (n != (len(l))):
@@ -93,17 +97,18 @@ if ((wks.row_values(d)[0]) == ''):
 
 n = 3
 
-'''
-EXAMPLE:
+
+(Old) EXAMPLE:
 worksheet.update_cell(1, 2, 'Bingo!')
 
-''' 
+#This is a function telling the value of cell A1:
+print (wks.cell(1, 1).value)
 
-#print (wks.cell(1, 1).value)
-
+#This is the old system to tell which row to insert on:
 while ((wks.row_values(d)[0]) != ''):
 	d += 1
 
+#This used to input values per cell in the row:
 for i in range((len(w)-3)):
 	if (n != (len(w))):
 		con = w[n]
@@ -113,7 +118,29 @@ for i in range((len(w)-3)):
 	if (n != (len(w))):
 		n += 1
 
+'''
 
-#(temp)print (d)
-#(temp)print (n)
-#(temp)print (con)
+#This is the new way the program determines which row to input on:
+try:
+
+	while ((wks.get_value((d,1))) != ''):
+		d += 1
+
+except pygsheets.exceptions.CellNotFound:
+	d += 1
+
+#This inserts new rows in, and also could be repeated multiple times because it increases the boundaries of the worksheet itself:
+wks.insert_rows((d-1), number = 1, values=value, inherit = False)
+
+#This gives a bold format to the first row of the worksheet, it can be disabled without any complications arising:
+try:
+	if (wks.get_value('Z1') == ''):
+		wks.update_cell('Z1', 'Bolded')
+		for i in range(1,10):
+			wks.cell((1,i)).set_text_format('bold', True)
+
+except googleapiclient.errors.HttpError:
+	hi = 'Hi Mom!'
+
+
+#This is the end, however some more edits may occur in the future.
